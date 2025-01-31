3dc899f9d2124f405050b040dfdc812640dd7b86
AmanBHRCH <amanhusain020202@gmail.com>
2025-01-31 16:36:59 +0530

    Project Added
    Services Project

---
+++ MVC-Project-1/src/main/java/com/controller/ForgotController.java
@@ -0,0 +1,32 @@
+package com.controller;
+
+import java.io.IOException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ForgotController")
+public class ForgotController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ForgotController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+	}
+
+}
